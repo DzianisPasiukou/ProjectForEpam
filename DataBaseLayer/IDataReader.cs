0d59d3103f3cc69cecb4145212fa8cf5607c3ece
DzianisPasiukou <d.pasukov@mail.ru>
2014-11-16 21:06:02 +0300

    Add database layer. Layer can add, update, delete, getdata from table.

---
+++ DataBaseLayer/IDataReader.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+
+namespace DBLabOne
+{
+  public  interface IDataReader
+    {
+        IEnumerable<object[]> GetData(string args);
+        bool Add(object obj);
+        bool Update(object obj);
+        bool Delete(object obj);
+    }
+}
