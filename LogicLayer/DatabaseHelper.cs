5881c5287265b842759189b91addac0556a62d67
Valentin <valicser@yandex.ru>
2014-11-17 12:54:41 +0300

    Add User model, Login model, Register model, logic with add new user

---
+++ LogicLayer/DatabaseHelper.cs
@@ -0,0 +1,73 @@
+﻿using DataBaseLayer;
+using DatabaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    public class DatabaseHelper : IDatabaseHelper
+    {
+        private IDataReader _dataReader;
+
+        public bool RegisterUser(string login, string password, string email, string name, string surname, string avatar)
+        {
+            if (String.IsNullOrEmpty(login))
+            {
+                throw new ArgumentNullException("login");
+            }
+            if (String.IsNullOrEmpty(password))
+            {
+                throw new ArgumentNullException("password");
+            }
+            if (String.IsNullOrEmpty(email))
+            {
+                throw new ArgumentNullException("email");
+            }
+            if (String.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+            if (String.IsNullOrEmpty(surname))
+            {
+                throw new ArgumentNullException("surname");
+            }
+            if (String.IsNullOrEmpty(avatar))
+            {
+                throw new ArgumentNullException("image");
+            }
+
+            Role role = new Role { RoleName = "Student" };
+
+            User user = new User { Login = login, Password = password, Email = email, IsActive = false, Name = name, Surname = surname, DateOfRegistration = DateTime.Now, RoleID = role.ID, Avatar = avatar };
+
+            return _dataReader.Add(user);
+
+        }
+
+        public User LoginUser(string login, string password)
+        {
+            if (String.IsNullOrEmpty(login))
+            {
+                throw new ArgumentNullException("login");
+            }
+            if (String.IsNullOrEmpty(password))
+            {
+                throw new ArgumentNullException("password");
+            }
+
+            User user = new User();
+
+            foreach (var item in _dataReader.GetData(String.Format("Login={1}", login)))
+            {
+                user.ID = (int)item.GetValue(0);
+                user.Login = (string)item.GetValue(3);
+                user.Password = (string)item.GetValue(4);
+            }
+
+            return user;
+        }
+    }
+}
