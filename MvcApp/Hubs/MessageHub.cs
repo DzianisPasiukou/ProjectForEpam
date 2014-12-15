f7ecde0b883f5bc5601658710911e3f5614a3121
MalakhovVladislav <malakhovvladislav@gmail.com>
2014-12-15 16:30:04 +0300

    add SignalR
    Startup.cs
    Hubs folder
    MessageHub

---
+++ MvcApp/Hubs/MessageHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MvcApp.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public void Send(string login,string message)
+        {
+            Clients.Client(login).Send(message);
+        }
+        public void Registr(string login)
+        {
+            Groups.Add(Context.ConnectionId, login);
+        }
+    }
+}
