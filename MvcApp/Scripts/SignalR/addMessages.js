0ea5a07b7b476e2194c51cf261cafb1894ed081c
MalakhovVladislav <malakhovvladislav@gmail.com>
2014-12-26 17:16:19 +0300

    chat
    add id to drop-message

--- MvcApp/Scripts/SignalR/addMessages.js
+++ MvcApp/Scripts/SignalR/addMessages.js
@@ -12,14 +12,13 @@
     chat.client.addToPage = function (senderLogin,message,date) {
         $('#chat-messages').append('<li class="right clearfix"><span class="chat-img pull-right"><img src="http://placehold.it/50/FA6F57/fff" alt="User Avatar" class="img-circle" /></span><div class="chat-body clearfix"><div class="header"><small class=" text-muted"><i class="fa fa-clock-o fa-fw"></i> '+date+'</small> <strong class="pull-right primary-font">'+senderLogin+'</strong> </div><p>'+message+' </p></div></li>');
     };
-    chat.client.send = function (senderLogin, message, date) {
-       
-        $('#chat-messages').append(' <li class="left clearfix"><span class="chat-img pull-left"><img src="http://placehold.it/50/55C1E7/fff" alt="User Avatar" class="img-circle" /></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">'+senderLogin+'</strong><small class="pull-right text-muted"><i class="fa fa-clock-o fa-fw"></i>'+date+'</small></div><p>'+message+'</p></div></li>');
+    chat.client.send = function (senderLogin, message, date) {     
+        $('#chat-messages').append(' <li class="left clearfix"><span class="chat-img pull-left"><img src="http://placehold.it/50/55C1E7/fff" alt="User Avatar" class="img-circle" /></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">' + senderLogin + '</strong><small class="pull-right text-muted"><i class="fa fa-clock-o fa-fw"></i>' + date + '</small></div><p>' + message + '</p></div></li>');
         //UpdateCount();
 
-        //if ($('#dropMessages').children().length == 6) {
+        //if ($('#dropdown-messages').children().length == 6) {
             
-        //    $('#dropMessages').find('li:first').next('li').next('li').next('li').remove();        
+        //    $('#dropdown-messages').find('li:first').next('li').next('li').next('li').remove();        
         //}     
     };
     $.connection.hub.start().done(function () {
